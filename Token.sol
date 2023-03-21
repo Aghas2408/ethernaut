bffa45fa34bf23f0a49eb5ac03adbaa4dea9e229
Aghas Seyranyan <aghasi.seyranyan@gmail.com>
2023-03-21 13:14:23 +0400

    Token finished

---
+++ Token.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+contract Token {
+
+  mapping(address => uint) balances;
+  uint public totalSupply;
+
+  constructor(uint _initialSupply) public {
+    balances[msg.sender] = totalSupply = _initialSupply;
+  }
+
+  function transfer(address _to, uint _value) public returns (bool) {
+    require(balances[msg.sender] - _value >= 0);
+    balances[msg.sender] -= _value;
+    balances[_to] += _value;
+    return true;
+  }
+
+  function balanceOf(address _owner) public view returns (uint balance) {
+    return balances[_owner];
+  }
+}
+
+// overflow doesn't checked in this contract, for this version of solidity we 
+// need to use SafeMath
+// await contract.transfer('0x0000000000000000000000000000000000000000', 21)
