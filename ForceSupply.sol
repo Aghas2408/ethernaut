ca99d75c93f55f81ae611b1f502f02af4b86c7f0
Aghas Seyranyan <aghasi.seyranyan@gmail.com>
2023-03-22 13:13:32 +0400

    Vault finished

--- ForceSupply.sol
+++ ForceSupply.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.6;
+pragma solidity ^0.6.0;
 
 contract ForceSupply {
     uint public balance = 0;
--- TelephoneCaller.sol
+++ TelephoneCaller.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.0;
 
 interface ITelephone {
     function changeOwner(address _owner) external;
