3936d391a448f94cb9d68fa5b7ffbf147ebe70f3
Aghas Seyranyan <aghasi.seyranyan@gmail.com>
2023-03-22 13:04:55 +0400

    Force finished

---
+++ ForceSupply.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6;
+
+contract ForceSupply {
+    uint public balance = 0;
+
+    function destruct(address payable _to) external payable {
+        selfdestruct(_to);
+    }
+
+    function deposit() external payable {
+        balance += msg.value;
+    }
+}
